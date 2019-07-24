c0f5ef58817e7c3872db094896a8d1692486ccb6
Trung <ungkientrung@gmail.com>
2019-07-24 18:52:18 +0300

    change back notification color

--- src/components/layouts/TodayNotification.js
+++ src/components/layouts/TodayNotification.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import today from '../utils/today';
-import formatDateString from '../utils/formatDateString';
-import morningQuote from '../utils/getRandomQuote';
+import React, { useState } from 'react'
+import today from '../utils/today'
+import formatDateString from '../utils/formatDateString'
+import morningQuote from '../utils/getRandomQuote'
 
 const TodayNotification = () => {
-  const [showTodayNoti, setShowTodayNoti] = useState(true);
+  const [showTodayNoti, setShowTodayNoti] = useState(true)
   return (
     <>
       {showTodayNoti && (
         <div className="section" style={{ paddingTop: '24px' }}>
           <div className="columns is-mobile is-multiline is-centered">
             <div className="column is-12-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
-              <div className="notification is-info has-text-centered">
+              <div className="notification is-danger has-text-centered">
                 <button
                   className="delete"
                   onClick={() => setShowTodayNoti(false)}
@@ -24,7 +24,7 @@ const TodayNotification = () => {
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default TodayNotification;
+export default TodayNotification
