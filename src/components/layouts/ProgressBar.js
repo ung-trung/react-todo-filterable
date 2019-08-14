4cd90b4b18ef681e2112e2edf4b599d0d8affb13
Trung <ungkientrung@gmail.com>
2019-08-14 19:30:13 +0300

    Progressbar animated

--- src/components/layouts/ProgressBar.js
+++ src/components/layouts/ProgressBar.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import getDisplayedTodos from '../utils/getDisplayedTodos';
+import React from 'react'
+import { connect } from 'react-redux'
+import getDisplayedTodos from '../utils/getDisplayedTodos'
+import { useSpring, animated } from 'react-spring'
 
 const ProgressBar = ({ displayTodos }) => {
   const getProgress = todos => {
+    if (todos.length === 0) {
+      return 100
+    }
     const numberCompletedTodos = todos.reduce(
       (acc, current) => (current.isCompleted ? ++acc : acc),
       0
-    );
+    )
 
-    if (isNaN(numberCompletedTodos / todos.length)) {
-      return 100;
-    }
-    return (numberCompletedTodos / todos.length) * 100;
-  };
+    return (numberCompletedTodos / todos.length) * 100
+  }
 
-  const value = getProgress(displayTodos);
+  const props = useSpring({
+    value: getProgress(displayTodos),
+    from: { value: 0 }
+  })
 
   return (
-    <>
-      <progress className="progress is-danger" value={value} max="100" />
-    </>
-  );
-};
+    <animated.progress
+      className="progress is-danger"
+      value={props.value}
+      max="100"
+    />
+  )
+}
 
 const mapStateToProps = state => ({
   displayTodos: getDisplayedTodos(
     state.todos.todos,
     state.form.daySort.values.selectedDay
   )
-});
+})
 
-export default connect(mapStateToProps)(ProgressBar);
+export default connect(mapStateToProps)(ProgressBar)
